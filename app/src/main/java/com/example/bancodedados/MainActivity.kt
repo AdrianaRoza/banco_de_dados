c522b2f09c030667c2922730218073a7ac83aef2
AdrianaRoza <adriana.roza.aka@gmail.com>
2023-04-18 16:29:39 -0300

    feat: retrieving list indexes

--- app/src/main/java/com/example/bancodedados/MainActivity.kt
+++ app/src/main/java/com/example/bancodedados/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.bancodedados
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.coroutines.DEBUG_PROPERTY_VALUE_AUTO
 
 class MainActivity : AppCompatActivity() {
@@ -18,11 +19,24 @@ class MainActivity : AppCompatActivity() {
             dataBase.execSQL("CREATE TABLE IF NOT EXISTS supplierS(name VARCHAR, telephone INT(4) )")
 
             //Inserir Dados em uma Tabela
-            dataBase.execSQL("INSERT INTO suppliers(name, telephone) VALUES ('Alexsandro', 1234)")
+            dataBase.execSQL("INSERT INTO suppliers(name, telephone) VALUES ('GOURMET SNACKS', 3697)")
 
             val query = "SELECT name, telephone FROM suppliers"
 
             val cursor = dataBase.rawQuery( query, null)
+
+            //Recuperar os Indices da Nossa Tabela
+            val indiceName = cursor.getColumnIndex("name")
+            val indiceTelephone = cursor.getColumnIndex("telephone")
+            cursor.moveToFirst()
+
+            while (cursor != null){
+
+                val name = cursor.getString(indiceName)
+                val telephone = cursor.getString(indiceTelephone)
+                Log.i("RESULT", "/name: $name / telephone: $telephone")
+                cursor.moveToNext()
+            }
 
         }catch (e: Exception){
             e.printStackTrace()
